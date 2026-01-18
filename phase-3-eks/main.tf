57929ca2cb417645a6d6b998e4ba9a6785b0e064
esulzickis <esulzickis@toogoodtogo.com>
2026-01-18 19:32:55 +0100

    Create eks cluster in one go

--- phase-3-eks/main.tf
+++ phase-3-eks/main.tf
@@ -15,8 +15,10 @@ module "vpc" {
   manage_default_security_group = false
   manage_default_route_table    = false
   manage_default_network_acl    = false
+  enable_nat_gateway            = true
 
   azs             = ["eu-west-1a", "eu-west-1b"]
+  public_subnets  = ["10.3.0.0/24"]
   private_subnets = ["10.3.1.0/24", "10.3.2.0/24"]
   tags            = local.tags
 }
@@ -25,7 +27,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.14.0"
 
-  name = "eks-cluster"
+  name               = "eks-cluster"
+  kubernetes_version = "1.34"
 
   control_plane_subnet_ids     = module.vpc.private_subnets
   subnet_ids                   = module.vpc.private_subnets
@@ -35,6 +38,39 @@ module "eks" {
   endpoint_private_access      = true
   endpoint_public_access       = true
   endpoint_public_access_cidrs = ["0.0.0.0/0"]
+
+  access_entries = {
+    sso_admin = {
+      principal_arn = var.iam_role_arn # The placeholder for your IAM role ARN to run kubectl commands
+      type          = "STANDARD"
+      policy_associations = {
+        cluster_admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
+  addons = {
+    vpc-cni = {
+      before_compute = true # Make sure VPC CNI is installed before compute nodes are created. Otherwise node health status will fail
+    }
+    # kube-proxy = {} # for pods to get IP addresses from the VPC
+  }
+
+  eks_managed_node_groups = {
+    first_node_group = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
 
   tags = local.tags
 }
